c111b8747f9ccf7aa60c30b656653e2501fea54f
EC2 Default User <ec2-user@ip-172-31-21-141.eu-west-1.compute.internal>
2023-04-18 13:51:35 +0000

    ebs

--- Health_Care/Health_Care/wsgi.py
+++ Health_Care/Health_Care/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Health_Care.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Health_Care.Health_Care.settings')
 
 application = get_wsgi_application()
